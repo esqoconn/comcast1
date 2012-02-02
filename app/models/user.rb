59e70bda387d830a99c1c61ae0f9cbeebddbcbf3
Sean OConnell <esqoconn@yahoo.com>
2012-02-01 16:28:50 -0800

    more clean up login page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,14 @@
 class User
   include MongoMapper::Document
   
+  key :name, String, :required => true
   key :email, String, :required => true
+  key :crypted_password, String
+  
+  def name=(new_name)
+    new_name.downcase! unless new_name.nil?
+    write_attribute(:name, new_name)
+  end  
   
   def email=(new_email)
     new_email.downcase! unless new_email.nil?
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Comcast1::Application.routes.draw do
   
     resources :users
+    resource  :session    
     
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -51,7 +52,7 @@ Comcast1::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'users#new'
 
   # See how all your routes lay out with "rake routes"
 
